eb71fd61c541114f8ee01a326927e14a5743fb04
lyzirving <lyzirving@sina.com>
2026-01-13 22:12:26 +0800

    add zlinq package.
    Signed-off-by: lyzirving <lyzirving@sina.com>

--- Assets/Scripts/Character/Player/State/PlayerStateRun.cs
+++ Assets/Scripts/Character/Player/State/PlayerStateRun.cs
@@ -2,20 +2,14 @@ using UnityEngine;
 
 public class PlayerStateRun : PlayerStateMove
 {
-    private EFootStep m_FootStep = EFootStep.None;
-
     public override void Enter(StateBase exitState, in ChangeStateArgs args)
     {
         base.Enter(exitState, args);
         m_Player.model.StartAnimation(m_Player.animConsts.runHash);
-        m_Player.model.RegisterLeftFootStepAction(OnLeftFootDown);
-        m_Player.model.RegisterRightFootStepAction(OnRightFootDown);
     }
 
     public override void Exit(StateBase newState)
     {
-        m_Player.model.RemoveLeftFootStepAction(OnLeftFootDown);
-        m_Player.model.RemoveRightFootStepAction(OnRightFootDown);
         m_Player.model.StopAnimation(m_Player.animConsts.runHash); 
         base.Exit(newState);
     }
@@ -47,15 +41,5 @@ public class PlayerStateRun : PlayerStateMove
         }              
 
         Move();
-    }
-
-    private void OnLeftFootDown()
-    {
-        m_FootStep = EFootStep.LeftFoot;
-    }
-
-    private void OnRightFootDown()
-    {
-        m_FootStep = EFootStep.RightFoot;
     }
 }
